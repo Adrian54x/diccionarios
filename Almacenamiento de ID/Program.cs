380faefd120c2540d563be1f2b2cd1f5301b7b7b
adria <adria@REX54X>
2026-03-05 10:32:58 -0600

    Agregar archivos de proyecto.

---
+++ Almacenamiento de ID/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Almacenamiento_de_ID
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int,string> id = new Dictionary<int,string>();
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write("Ingrese ID del estudiante:");
+                int id_estudiante = int.Parse(Console.ReadLine());
+                Console.Write("Ingrese ID del estudiante:");
+                id.Add();
+            }
+        }
+    }
+}
